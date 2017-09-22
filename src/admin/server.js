51f2672d0f667d0a022701be1b6fb6babd422877
lmm <1968765605@qq.com>
2017-09-22 11:10:26 +0800

    文件上传完成

--- src/admin/server.js
+++ src/admin/server.js
@@ -1,134 +1,168 @@
 //引入http模块  mysql模块
-const http = require('http');
-const mysql = require('mysql');	//第三方模块
+// const http = require('http');
+// const mysql = require('mysql');	//第三方模块
+var express = require("express");
+var app = express();
+var multer = require("multer");
+//配置上传文件存放的信息
+var storage = multer.diskStorage({
+	//设置上传文件存放的目录
+	destination: function(req, file, cb) {
+		cb(null, '../img/oplist')
+	},
+	//设置上传后文件的名字
+	filename: function(req, file, cb) {
+		//拼接文件名和文件后缀
+		var fileFormat = file.originalname.split(".");
+		cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1])
+	}
+})
+//往multer去配置这个存放文件的信息
+var upload = multer({
+	storage: storage
+})
 
-//查询字符串，一般是对http请求所带的数据进行解析
-//有4个方法 现在用querystring.parse()  字符串转化为一个对象
-const querystring = require('querystring');
-
-//利用url.parse()方法，将一个url的字符串地址解析并返回一个url对象
-//url.parse方法里有一个pathname属性，利用这个属性判断对应的页面作出不同的操作
-const url = require('url');
-
-//连接数据库  createConnection()方法
-var connection = mysql.createConnection({
-
-	host:'localhost',
-	user:'root',
-	password:'',
-	database:'outlets'
-});
-
-//连接数据库
-connection.connect();
-
-//创建服务器 createServer(fn(req,res))
-//解析url的专用模块
-//request是一个stream流
-http.createServer(function(req,res){
-
-	//存放流数据
-	var post = '';
-
-	req.on('data',function(chunk){
-		//不断写入流数据
-		post += chunk;
+//多文件上传
+app.post('/profile', upload.any(), function(req, res, next) {
+	var ress =[];
+	req.files.map(function(item){
+		ress.push(item.filename)
 	});
+	console.log(ress)
+	res.append('Access-Control-Allow-Origin','*');
+	res.send(ress); 
+})
 
-	//获取数据结束
-	req.on('end',function(){
+//监听端口，并打开服务器
+app.listen(12346);
+console.log("开启服务器")
+// //查询字符串，一般是对http请求所带的数据进行解析
+// //有4个方法 现在用querystring.parse()  字符串转化为一个对象
+// const querystring = require('querystring');
 
-		//获取url的路径名
-		var pathname = url.parse(req.url).pathname;
+// //利用url.parse()方法，将一个url的字符串地址解析并返回一个url对象
+// //url.parse方法里有一个pathname属性，利用这个属性判断对应的页面作出不同的操作
+// const url = require('url');
 
-		//获取ports  前端在ajax(data 参数) 里传入的数据  是一个对象
-		var ports = querystring.parse(post);console.log(ports)
+// //连接数据库  createConnection()方法
+// var connection = mysql.createConnection({
 
-		//设置请求头
-		res.setHeader('Access-Control-Allow-Origin','*');
+// 	host:'localhost',
+// 	user:'root',
+// 	password:'',
+// 	database:'outlets'
+// });
 
-		//判断路径名 作出不同的处理
-		switch(pathname){
+// //连接数据库
+// connection.connect();
 
-			//查询全部（查）
-			case '/select':
-				connection.query('select * from goods',function(error,results,fields){
-					if(error) throw error;
+// //创建服务器 createServer(fn(req,res))
+// //解析url的专用模块
+// //request是一个stream流
+// http.createServer(function(req,res){
+
+// 	//存放流数据
+// 	var post = '';
+
+// 	req.on('data',function(chunk){
+// 		//不断写入流数据
+// 		post += chunk;
+// 	});
+
+// 	//获取数据结束
+// 	req.on('end',function(){
+
+// 		//获取url的路径名
+// 		var pathname = url.parse(req.url).pathname;
+
+// 		//获取ports  前端在ajax(data 参数) 里传入的数据  是一个对象
+// 		var ports = querystring.parse(post);console.log(ports)
+
+// 		//设置请求头
+// 		res.setHeader('Access-Control-Allow-Origin','*');
+
+// 		//判断路径名 作出不同的处理
+// 		switch(pathname){
+
+// 			//查询全部（查）
+// 			case '/select':
+// 				connection.query('select * from goods',function(error,results,fields){
+// 					if(error) throw error;
 					
-					//向前端发送请求到的数据，并且转成JSON字符串
-					res.end(JSON.stringify({
-						status:1,
-						results
-					}))
-				});
-				break;
+// 					//向前端发送请求到的数据，并且转成JSON字符串
+// 					res.end(JSON.stringify({
+// 						status:1,
+// 						results
+// 					}))
+// 				});
+// 				break;
 
-			//插入（增）
-			case '/insert':
+// 			//插入（增）
+// 			case '/insert':
 
-				connection.query(`insert into goods (name,qty,price,our_price) values ('${ports.name}','${ports.qty}','${ports.price}','${ports.our_price}')`,function(error,results,fields){
-					if(error) throw error;
+// 				connection.query(`insert into goods (name,qty,price,our_price) values ('${ports.name}','${ports.qty}','${ports.price}','${ports.our_price}')`,function(error,results,fields){
+// 					if(error) throw error;
 
-					//向前端发送请求到的数据，并且转成JSON字符串
-					res.end(JSON.stringify({
-						status:'增加成功',
-						results
-					}))
-				});
-				break;
+// 					//向前端发送请求到的数据，并且转成JSON字符串
+// 					res.end(JSON.stringify({
+// 						status:'增加成功',
+// 						results
+// 					}))
+// 				});
+// 				break;
 
-			//删除单个
-			case '/delete':
-				connection.query(`DELETE FROM goods where id=${ports.goodsId}`,function(error,results,fields){
-					if (error) throw error;
+// 			//删除单个
+// 			case '/delete':
+// 				connection.query(`DELETE FROM goods where id=${ports.goodsId}`,function(error,results,fields){
+// 					if (error) throw error;
 
-					res.end(JSON.stringify({
-						status:1,
-						results
-					}))
-				});
-				break;
+// 					res.end(JSON.stringify({
+// 						status:1,
+// 						results
+// 					}))
+// 				});
+// 				break;
 
-			//查询 input框关键字
-			case '/search':
-				connection.query(`SELECT * from goods WHERE CONCAT(name,qty,price,our_price) like '${ports.searchtext}%'`,function(error,results,fields){
-					if (error) throw error;
+// 			//查询 input框关键字
+// 			case '/search':
+// 				connection.query(`SELECT * from goods WHERE CONCAT(name,qty,price,our_price) like '${ports.searchtext}%'`,function(error,results,fields){
+// 					if (error) throw error;
 
-					res.end(JSON.stringify({
-						status:1,
-						results
-					}))
-				});
-				break;
+// 					res.end(JSON.stringify({
+// 						status:1,
+// 						results
+// 					}))
+// 				});
+// 				break;
 
-			//查询当前id的信息（查询当前id对应的商品 把内容填充到input里）
-			case '/haveid':
-				connection.query(`select * FROM goods where id=${ports.goodsId}`,function(error,results,fields){
-					if (error) throw error;
+// 			//查询当前id的信息（查询当前id对应的商品 把内容填充到input里）
+// 			case '/haveid':
+// 				connection.query(`select * FROM goods where id=${ports.goodsId}`,function(error,results,fields){
+// 					if (error) throw error;
 
-					res.end(JSON.stringify({
-						status:1,
-						results
-					}))
+// 					res.end(JSON.stringify({
+// 						status:1,
+// 						results
+// 					}))
 
-				});
-				break;
+// 				});
+// 				break;
 
-			//改  username = 字符串
-			case '/update':
-				connection.query(`UPDATE goods SET name="${ports.name}",qty="${ports.qty}",price="${ports.price}",our_price="${ports.our_price}" WHERE id=${ports.goodsId}`,function(error,results,fields){
-					if (error) throw error;
+// 			//改  username = 字符串
+// 			case '/update':
+// 				connection.query(`UPDATE goods SET name="${ports.name}",qty="${ports.qty}",price="${ports.price}",our_price="${ports.our_price}" WHERE id=${ports.goodsId}`,function(error,results,fields){
+// 					if (error) throw error;
 
-					res.end(JSON.stringify({
-						status:'修改成功',
-						results
-					}))
-				});
+// 					res.end(JSON.stringify({
+// 						status:'修改成功',
+// 						results
+// 					}))
+// 				});
 				
-		}
-	})
+// 		}
+// 	})
 
-}).listen(8888)
+// }).listen(8888)
 
-console.log('server start!')
+// console.log('server start!')
 
